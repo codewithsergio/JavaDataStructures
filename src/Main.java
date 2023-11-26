47b4894ce86a9639b7f70834a8e2c26e2e323c00
Sergio Hernandez <ehernandez.sergio@gmail.com>
2023-11-26 13:05:54 -0800

    handled conflict

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        
-=======
-        Trie trie = new Trie();
-        trie.insert("book");
-        trie.insert("border");
-        trie.insert("cat");
-        trie.insert("dog");
-        trie.insert("doctor");
-        trie.insert("fine");
-        trie.insert("finest");
-        trie.insert("figure");
 
-        int a = trie.getWordCount();
-        System.out.println(a);
-
-        trie.printWords();
->>>>>>> 98140578ce5bbef1008b69f55fb3eb88758ebee7
     }
 }
