67565caafc6600c05ee26da6e3e2ccbbdf5328a1
Sergio Hernandez <ehernandez.sergio@gmail.com>
2023-11-26 13:54:20 -0800

    MyLinkedList class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
+        MyLinkedList list = new MyLinkedList();
+        list.insertAtEnd(6);
+        list.insertAtEnd(3);
+        list.insertAtEnd(10);
+        list.insertAtEnd(23);
+        list.insertAtEnd(9);
+        list.insertAtEnd(16);
+        list.insertAtEnd(89);
+        list.reverse();
+        System.out.println(list);
     }
 }
