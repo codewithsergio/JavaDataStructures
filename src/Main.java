561468fcd0dfd3d48885c783eff7ee33b666f034
Sergio Hernandez <ehernandez.sergio@gmail.com>
2023-11-26 13:01:49 -0800

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Trie trie = new Trie();
-        trie.insert("calling");
-        trie.insert("cat");
-        trie.insert("dog");
-        trie.insert("cattle");
-        trie.insert("air");
-        trie.insert("duck");
-        trie.insert("cut");
+        
     }
 }
