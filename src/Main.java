72daae8d626eef8d623c0865fe097a8515d0504d
Sergio Hernandez <ehernandez.sergio@gmail.com>
2023-11-27 20:48:28 -0800

    testing

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(9);
         tree.insert(5);
-        tree.insert(3);
         tree.insert(1);
-        tree.insert(7);
+        tree.insert(8);
+        tree.insert(6);
+        tree.insert(12);
         tree.insert(10);
-        tree.insert(4);
-        int r = tree.kthSmallest(4);
+        tree.insert(16);
+        int r = tree.kthSmallest(5);
         System.out.println(r);
     }
 }
