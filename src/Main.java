1acc837544c10de9e21dc5f18c3bbc9843d2aae0
Sergio Hernandez <ehernandez.sergio@gmail.com>
2023-11-27 17:21:49 -0800

    MyHeap class

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
+import java.sql.SQLOutput;
+
 public class Main {
-    public static void main(String[] args) {
-        Trie trie = new Trie();
-        trie.insert("book");
-        trie.insert("border");
-        trie.insert("cat");
-        trie.insert("dog");
-        trie.insert("doctor");
-        trie.insert("fine");
-        trie.insert("finest");
-        trie.insert("figure");
-
-        int a = trie.getWordCount();
-        System.out.println(a);
-
-        trie.printWords();
+    public static void main(String[] args) throws Exception {
+        MyHeap heap = new MyHeap(9);
+        heap.insert(15);
+        heap.insert(10);
+        heap.insert(3);
+        heap.insert(8);
+        heap.insert(12);
+        heap.insert(9);
+        heap.insert(4);
+        heap.insert(1);
+        heap.insert(24);
+        heap.remove();
+        System.out.println(heap);
     }
 }
+// functions to add
+// isComplete() BST
